522d738179ff6c5172062fdce930ca95b112102e
nahashon <nashonmwendwa0@gmail.com>
2025-06-28 10:15:26 +0300

    created a footer and add basic code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p><span title="MymOVIES"></span>MymOVIES</p>
+        <p> <em>Fmovies</em> - Free movies online, here you can watch movies online in high quality for free without annoying of advertising, just come and enjoy your movies online. fmovie, fmovies, bmovies
+            Disclaimer: This site does not store any files on its server. All contents are provided by non-affiliated third parties.</p>
+
+    </div>
+  )
+}
+
+export default Footer
